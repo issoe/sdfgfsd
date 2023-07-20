88a35e87e0a3cbdfd9cd34546c2ef8d5c8e7a5c0
Khanh, Pham Quang <khanhpqz@nec.vn>
2023-07-20 17:02:20 +0700

    first commit for socket java

---
+++ src/main/java/com/learn/sockett/SockettApplication.java
@@ -0,0 +1,16 @@
+package com.learn.sockett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockettApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockettApplication.class, args);
+	}
+
+}
+
+
+// https://viblo.asia/p/tao-ung-dung-chat-don-gian-voi-spring-boot-va-websocket-naQ
---
+++ src/test/java/com/learn/sockett/SockettApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.sockett;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockettApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
